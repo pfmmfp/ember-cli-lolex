b655c2e22a1bd9aaaddba1ed392eb5c643fffa1e
Cruz Fernandez <cruz@scvsoft.com>
2017-01-11 14:08:12 -0300

    Added shim for lolex, bower reference, acceptance test that just loads the lolex
    Also replaced generated Ember.assign with primitives for running on Ember 2.4

--- index.js
+++ index.js
@@ -2,5 +2,14 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-lolex'
+  name: 'ember-cli-lolex',
+  included: function included(app) {
+    this._super.included(app);
+    app.import(app.bowerDirectory + '/lolex/lolex.js');
+    app.import('vendor/shims.js', {
+      exports: {
+        lolex: ['default']
+      }
+    });
+  }
 };
